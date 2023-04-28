4bea88c29d7ec37f523301cab1f9e4cb0be934d1
jeremiahtaylor2021 <jeremiahtaylor2021@gmail.com>
2023-04-27 17:32:09 -0700

    initial commit

---
+++ cmd/web/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
+	"github.com/jeremiahtaylor2017/bookings/pkg/config"
+	"github.com/jeremiahtaylor2017/bookings/pkg/handlers"
+	"github.com/jeremiahtaylor2017/bookings/pkg/render"
+)
+
+const port = ":8080"
+
+var app config.AppConfig
+var session *scs.SessionManager
+
+// main is the main application function
+func main() {
+	// changfe this to true when in production
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
+
+	templateCache, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+
+	app.TemplateCache = templateCache
+	// Useful for development mode so it reads from disk
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplates(&app)
+
+	fmt.Printf("Starting application on port %s\n", port)
+
+	srv := &http.Server{
+		Addr:    port,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	log.Fatal(err)
+}
